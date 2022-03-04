29879de965b27b0c5a5e2b9eaa13c5e4191bc771
Himandri Sharma <himandrisharma27@gmail.com>
2022-03-04 17:51:52 +0530

    debugging

--- src/index.js
+++ src/index.js
@@ -6,28 +6,28 @@ import {HashRouter} from 'react-router-dom';
 
 const rootElement = document.getElementById("root");
 
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>, 
-    rootElement);
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.StrictMode>,
-    rootElement);
-}
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+// if (rootElement.hasChildNodes()) {
+//   ReactDOM.hydrate(
+//     <React.StrictMode>
+//       <HashRouter>
+//         <App />
+//       </HashRouter>
+//     </React.StrictMode>, 
+//     rootElement);
+// } else {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <HashRouter>
+//         <App />
+//       </HashRouter>
+//     </React.StrictMode>,
+//     rootElement);
+// }
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
