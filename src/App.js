a15f68f09aa98164f0ab00dee70d71517032cde7
Himandri Sharma <himandrisharma27@gmail.com>
2022-09-12 22:40:45 +0530

    google analytics added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
 /* eslint-disable no-lone-blocks */
-import './App.css';
-import Home from './components/Desktop/home';
-import Experience from './components/Desktop/experience';
-import Socials from './components/Desktop/Socials';
-import MobileHome from './components/Mobile/MobileHome';
-import MobileExperience from './components/Mobile/MobileExperience';
-import MobileSocials from './components/Mobile/MobileSocials';
-import NoneHome from './components/None/NoneHome';
-import NoneExperience from './components/None/NoneExperience';
-import NoneSocials from './components/None/NoneSocials';
-import useWindowDimensions from './useWindowDimensions';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import "./App.css";
+import Home from "./components/Desktop/home";
+import Experience from "./components/Desktop/experience";
+import Socials from "./components/Desktop/Socials";
+import MobileHome from "./components/Mobile/MobileHome";
+import MobileExperience from "./components/Mobile/MobileExperience";
+import MobileSocials from "./components/Mobile/MobileSocials";
+import NoneHome from "./components/None/NoneHome";
+import NoneExperience from "./components/None/NoneExperience";
+import NoneSocials from "./components/None/NoneSocials";
+import useWindowDimensions from "./useWindowDimensions";
+import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-163181774-1"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 function App() {
   const ratio = useWindowDimensions();
-  const isDesktop = ratio > 1.67 ;
-  const isMobile = ratio < 0.88 ;
+  const isDesktop = ratio > 1.67;
+  const isMobile = ratio < 0.88;
   return (
     <div className="App">
-    {isDesktop && (
-      <Routes>
-      <Route path="/" exact element={<Home/>}/>
-      <Route path="/About" exact element={<Experience/>}/>
-      <Route path="/Socials" exact element={<Socials/>}/>
-    </Routes>
-    )
-    }
-    {(isMobile && (
-      <Routes>
-        <Route path="/" exact element={<MobileHome/>}/>
-        <Route path="/About" exact element={<MobileExperience/>}/>
-        <Route path="/Socials" exact element={<MobileSocials/>}/>
-      </Routes>
-    ))}
-    {(!isMobile && !isDesktop && (
-      <Routes>
-        <Route path="/" exact element={<NoneHome/>}/>
-        <Route path="/About" exact element={<NoneExperience/>}/>
-        <Route path="/Socials" exact element={<NoneSocials/>}/>
-      </Routes>
-    ))}
+      {isDesktop && (
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/About" exact element={<Experience />} />
+          <Route path="/Socials" exact element={<Socials />} />
+        </Routes>
+      )}
+      {isMobile && (
+        <Routes>
+          <Route path="/" exact element={<MobileHome />} />
+          <Route path="/About" exact element={<MobileExperience />} />
+          <Route path="/Socials" exact element={<MobileSocials />} />
+        </Routes>
+      )}
+      {!isMobile && !isDesktop && (
+        <Routes>
+          <Route path="/" exact element={<NoneHome />} />
+          <Route path="/About" exact element={<NoneExperience />} />
+          <Route path="/Socials" exact element={<NoneSocials />} />
+        </Routes>
+      )}
     </div>
   );
 }
--- src/components/Desktop/home.js
+++ src/components/Desktop/home.js
@@ -1,17 +1,39 @@
-import './home.css';
-import side from '../../images/GameBoard.svg';
-import Title from '../../AnimatedText/Title';
-import {Link} from 'react-router-dom';
+import "./home.css";
+import side from "../../images/GameBoard.svg";
+import Title from "../../AnimatedText/Title";
+import { Link } from "react-router-dom";
+import useAnalyticsEventTracker from "../../useAnalyticsEventTracker";
 
 function Home() {
+  const gaEventTracker = useAnalyticsEventTracker("Socials");
   return (
     <div className="HomeStyle">
-      <div className="navbar"><a className="navIcon" href="https://drive.google.com/file/d/1UAVbtYNI1qz1kFOnxyzK-AlfNoDpBhTU/view"  >Resume</a>&nbsp;&nbsp;&nbsp;<Link className="navIcon" to="/About"  >About</Link>&nbsp;&nbsp;&nbsp;<Link className="navIcon" to="/Socials"  >Socials</Link> </div>
-      <img alt="" src={side} className="HomeImage"/>
+      <div className="navbar">
+        <a
+          className="navIcon"
+          onClick={()=>gaEventTracker('resume')}
+          href="https://drive.google.com/file/d/1UAVbtYNI1qz1kFOnxyzK-AlfNoDpBhTU/view"
+        >
+          Resume
+        </a>
+        &nbsp;&nbsp;&nbsp;
+        <Link className="navIcon" to="/About">
+          About
+        </Link>
+        &nbsp;&nbsp;&nbsp;
+        <Link className="navIcon" to="/Socials">
+          Socials
+        </Link>{" "}
+      </div>
+      <img alt="" src={side} className="HomeImage" />
       <div className="Text">
         <div className="HomeSubText">Hello 👋🏼, I am</div>
-        <Title text="Himandri"/>
-        <div className="HomeParaText">I am a Front-End Developer who loves connecting with people<br/> through tech communities and learn new tech<br/> stacks to build cool things!</div>
+        <Title text="Himandri" />
+        <div className="HomeParaText">
+          I am a Front-End Developer who loves connecting with people
+          <br /> through tech communities and learn new tech
+          <br /> stacks to build cool things!
+        </div>
       </div>
     </div>
   );
