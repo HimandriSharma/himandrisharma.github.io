8079205e2cc5df0fad869845a434665e2b676375
Himandri Sharma <43072879+HimandriSharma@users.noreply.github.com>
2023-12-12 16:16:48 +0530

    Update MobileHome.js

--- src/components/Mobile/MobileHome.js
+++ src/components/Mobile/MobileHome.js
@@ -6,7 +6,8 @@ import {Link} from 'react-router-dom';
 function MobileHome() {
   return (
     <div className="MobileHomeStyle">
-        <div className="MobileNavbar"><a className="navIcon" href="https://drive.google.com/file/d/1ueZlkiE-17-scAGr5rY0Yk1ieeIKC8cX/view?usp=sharing"  >Resume</a>&nbsp;&nbsp;&nbsp;<Link className="navIcon" to="/About"  >About</Link>&nbsp;&nbsp;&nbsp;<Link className="navIcon" to="/Socials"  >Socials</Link> </div>
+        <div className="MobileNavbar"><a className="navIcon" href="https://drive.google.com/file/d/1ueZlkiE-17-scAGr5rY0Yk1ieeIKC8cX/view?usp=sharing"  >Resume</a>&nbsp;&nbsp;&nbsp;<Link className="navIcon" to="/Socials"  >Socials</Link> </div>
+// <Link className="navIcon" to="/About"  >About</Link>&nbsp;&nbsp;&nbsp;
         <img src={GameBoard} alt="" className="MobileHomeImage"/>
         <div className="MobileHomeText">
             <div className="MobileHomeSubText">Hello 👋🏼, I am</div>
